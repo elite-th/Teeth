d57b7e075d012228f47684cd9c09b52af4d4b222
elite-th <elite-th@users.noreply.github.com>
2026-04-27 01:06:44 +0000

    chore: remove non-Lumina files from repo (skills, examples, env configs, etc.)

--- keep-server-alive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-    cd /home/z/my-project
-    node .next/standalone/server.js
-    echo "Server crashed, restarting in 3 seconds..." >> /home/z/my-project/dev.log
-    sleep 3
-done
